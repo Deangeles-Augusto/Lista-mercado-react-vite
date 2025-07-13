44e8eb2b593e18d0297b3a2c0baa4b0389aa1e8e
Deangeles Augusto <deangeles_augusto@hotmail.com>
2025-07-13 15:04:00 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import ItemLista from "./ItemLista";
+import { useRef, useState } from "react";
+
+function App() {
+  const [listaMercado, setListaMercado] = useState([]);
+
+  const inputAdicionar = useRef();
+
+  //const listaMercado = ["Banana", "Maçã", "Carne"];
+  //listaMercado = []
+
+  //Hook
+  //useState - Cria uma variável de estado
+  //Ele não retorna uma informação
+  //Ele retorna um array [a variável que armazena a informação, uma função para alterar essa variável]
+
+  const adicionarElementoNaLista = () => {
+    const novaLista = [...listaMercado];
+    const valorInput = inputAdicionar.current.value;
+
+    if (valorInput) {
+      novaLista.push(valorInput);
+      setListaMercado(novaLista);
+
+      inputAdicionar.current.value = "";
+    }
+  };
+
+  return (
+    <div className="flex w-full max-w-96 flex-col items-center gap-4">
+      <h1 className="text-3xl font-bold">Lista de Mercado</h1>
+
+      <div className="flex w-full gap-2">
+        <input
+          className="w-full border border-gray-600 rouded-md px-2"
+          ref={inputAdicionar}
+          type="text"
+          placeholder="Digite um item"
+        />
+        <button className="rounded-md bg-gray-800 text-white px-2 cursor-pointer hover:bg-gray-600 transition" onClick={() => adicionarElementoNaLista()}>Adicionar</button>
+      </div>
+
+      {listaMercado.length > 0 ? (
+        <ul className="w-full flex flex-col gap-2">
+          {listaMercado.map((itemLista, index) => (
+            <ItemLista
+              key={index}
+              itemLista={itemLista}
+              listaMercado={listaMercado}
+              setListaMercado={setListaMercado}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>Você não tem nenhum item na sua lista</p>
+      )}
+    </div>
+  );
+  // tag vazia em react se chama "Fragment"
+}
+
+export default App;
